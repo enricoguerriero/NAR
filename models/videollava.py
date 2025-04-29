753bcc120625426f5527998c44dee4e62c718a21
enricoguerriero <enrico.guerriero@gmail.com>
2025-04-29 10:27:01 +0200

    db

--- models/videollava.py
+++ models/videollava.py
@@ -166,7 +166,7 @@ class VideoLlava(BaseModel):
                 inputs = {k: v.to(self.device) for k, v in batch.items()}
                 labels = inputs.pop("labels")
                 
-                outputs = self.forward_classifier(**inputs, loss_fct=loss_fct)
+                outputs = self.forward_classifier(**inputs, labels = labels, loss_fct=loss_fct)
                 loss = outputs["loss"]
                 logits = outputs["logits"]
                 labels_list.append(labels)
