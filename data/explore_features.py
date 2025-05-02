a652cdbc98345c25ae8afdbae0970181420997b2
enricoguerriero <enrico.guerriero@gmail.com>
2025-05-02 15:54:41 +0200

    db

--- data/explore_features.py
+++ data/explore_features.py
@@ -27,6 +27,8 @@ def main(data_dir: str):
     # iterate through dataset
     for item in dataset:
         labels = item['labels'].squeeze(0)
+        print(f"Labels shape: {labels.shape}", flush=True)
+        print(f"Labels: {labels.tolist()}", flush=True)
         pos_counts += labels.to(torch.int64)
         for k in shape_counters:
             tensor = item[k]
