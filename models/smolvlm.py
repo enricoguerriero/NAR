dbeba26001ff5092072747cb31a413c1804b8ad3
enricoguerriero <enrico.guerriero@gmail.com>
2025-05-08 19:07:53 +0200

    db

--- models/smolvlm.py
+++ models/smolvlm.py
@@ -40,6 +40,7 @@ class SmolVLM(BaseModel):
         
         self.backbone = get_peft_model(self.backbone, lora_config)
         
+        print(self.backbone.config)
         hidden_size = self.backbone.config.hidden_size
         self.classifier = nn.Sequential(
             nn.Linear(hidden_size, 512),
